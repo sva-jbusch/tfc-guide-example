b0d6f4d48a02935c2eb11a94f5b84570ce0a3de3
sva-jbusch <77288420+sva-jbusch@users.noreply.github.com>
2021-09-08 10:48:21 -0500

    Added NIC to instance

--- main.tf
+++ main.tf
@@ -18,9 +18,24 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_network_interface" "foo" {
+  subnet_id   = "subnet-0edd5ad189eac4c77"
+#  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+  availability_zone = "us-west-2a"
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+
 
   tags = {
     Name = var.instance_name
